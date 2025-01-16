087068695f5e1730760ebee7b64416ebfa72cc9b
Miguel Castro <54249734+migueacastro@users.noreply.github.com>
2025-01-16 18:11:01 +0000

    JWT auth Working

--- backend/dimpro/admin.py
+++ backend/dimpro/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from dimpro.models import *
+#
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin): # Esto es para ver detalladamente la lista
+  list_display = ('id', 'item', 'details', 'reference', 'prices', 'available_quantity')
+  list_filter = ('active',)
+  search_fields = ('item', 'details', 'reference')
+  list_per_page = 25 
+
+class UserAdmin(admin.ModelAdmin):
+  list_display = ('id', 'email', 'name', 'phonenumber', 'active', 'is_superuser')
+  list_filter = ('active', 'is_superuser')
+  search_fields = ('email', 'name')
+  list_per_page = 25
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Contact) # Esto es para ver solo los registros por el id, no muestra detalles a simple vista
--- backend/dimpro/models.py
+++ backend/dimpro/models.py
@@ -1,6 +1,3 @@
-from django.db import models
-
-# Create your models here.
 from typing import Any
 from django.contrib.auth.models import UserManager, AbstractBaseUser, PermissionsMixin, timezone
 from django.contrib.auth.hashers import check_password
@@ -10,11 +7,11 @@ from phonenumber_field.modelfields import PhoneNumberField
 from django.core.validators import MaxValueValidator, MinValueValidator, RegexValidator
 import datetime
 # Create your models here.
-class CustomUserManager(UserManager):
+class CustomUserManager(UserManager): 
     def _create_user(self, email, password, phonenumber, **extra_fields):
         if not email:
             raise ValueError("No has ingresado una direccion e-mail valida.")
-        
+
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
@@ -22,35 +19,35 @@ class CustomUserManager(UserManager):
         user.save(using=self._db)
 
         return user
-    
+
     def create_user(self, email=None, password=None, phonenumber=None, **extra_fields):
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
         extra_fields.setdefault('is_operator', False)
         return self._create_user(email, password, phonenumber, **extra_fields)
-    
+
     def create_superuser(self, email=None, password=None, phonenumber=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_operator', True)
         return self._create_user(email, password, phonenumber,**extra_fields)
-    
+
     def create_staff(self, email=None, password=None, phonenumber=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', False)
         extra_fields.setdefault('is_operator', False)
         return self._create_user(email, password, phonenumber,**extra_fields)
-    
-class User(AbstractBaseUser, PermissionsMixin):
+
+class User(AbstractBaseUser, PermissionsMixin): 
     id = models.AutoField(primary_key=True)
     email = models.EmailField(blank=True, default='', unique=True)
     name = models.CharField(max_length=255, blank=True, default='')
     last_name = models.CharField(max_length=255, blank=True, default='')
     phoneregex = RegexValidator(regex=r"^\+?58?\d{11,15}$")
     phonenumber = models.CharField(
-        validators=[phoneregex], max_length=17, blank=True, null=False
+        validators=[phoneregex], max_length=17, blank=True, null=True
     )
-    is_active = models.BooleanField(default=True)
+    active = models.BooleanField(default=True) # lo modifique para que se adapte al SafeViewSet
     is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_operator = models.BooleanField(default=False)
@@ -66,76 +63,101 @@ class User(AbstractBaseUser, PermissionsMixin):
     class Meta:
         verbose_name = 'User'
         verbose_name_plural = 'Users'
-    
+
     def get_full_name(self):
         return self.name
-    
+
     def get_short_name(self):
         return self.name or self.email.split('@')[0]
-    
+
     def user_orders(self):
         return Order.objects.filter(user_email=self.id).count()
-    
-class Product(models.Model):
+
+class Product(models.Model): 
     item = models.CharField(max_length=64, unique=False)
-    id = models.AutoField(primary_key=True)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    active = models.BooleanField(null = False, default=True) 
+    prices = models.JSONField(blank=True, default=dict)
     details = models.CharField(max_length=128, null = True)
     reference = models.CharField(max_length=64, unique=True, null = True)
     available_quantity = models.IntegerField(validators = [
         MinValueValidator(1)
     ])
+
     def __str__(self):
         return str(self.id)
 
+    # Create a dictionary based on the product
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'item': self.item,
+            'details': self.details,
+            'reference': self.reference,
+            'prices': self.prices,
+            'available_quantity': self.available_quantity
+        }
+
 class Image(models.Model):
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
     url = models.CharField(max_length=256)
 
 class AlegraUser(models.Model):
     email = models.CharField(max_length=128)
     token = models.CharField(max_length=256)
 
-    
-class Contact(models.Model):
+
+class Contact(models.Model): 
     name = models.CharField(max_length=128)
     date_joined = models.DateTimeField(auto_now_add=True)
-    active = models.BooleanField(null = False)
+    active = models.BooleanField(null = False, default=True)
     def __str__(self):
         return str(self.id)
-    
+
+# TODO: viewset para Order
 class Order(models.Model):
-    user_email = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    user_email = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders', null=True)
     status = models.CharField(max_length=16,choices= [
         ('preparado', 'Preparado'),
         ('pendiente', 'Pendiente')
     ])
-    client_id = models.ForeignKey(Contact, on_delete=models.CASCADE, related_name='orders')
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE, related_name='orders', null=True)
     date = models.DateTimeField(auto_now_add=True)
     total = models.DecimalField(max_digits=7, decimal_places=2)
-    type = models.CharField(max_length=16,choices= [
-        ('nota de entrega', 'Nota de entrega'),
-        ('factura', 'Factura')
-    ])
+    pricetype = models.CharField(max_length=128, null=True)
+    active = models.BooleanField(null = False, default=True)
     def product_categories(self):
         return Order_Product.objects.filter(order_id=self.id).count()
-    
+
     def __str__(self):
         return str(self.id)
 
 
+class PriceType(models.Model):
+    name = models.CharField(max_length = 128)
+    default = models.BooleanField(default=False)
+
 
 class Order_Product(models.Model):
     id = models.AutoField(primary_key=True)
-    order_id =  models.ForeignKey(Order, on_delete=models.CASCADE, related_name='orders')
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='orders')
-    cost = models.DecimalField(max_digits=7,decimal_places=2)
+    order =  models.ForeignKey(Order, on_delete=models.CASCADE, related_name='orders')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='orders')
+    price = models.DecimalField(max_digits=7,decimal_places=2, default=0)
+    cost = models.DecimalField(max_digits=7,decimal_places=2, default=0)
     quantity = models.IntegerField(validators = [
         MinValueValidator(1)
     ])
     def __str__(self):
         return str(self.id)
-    @property
-    def price(self):
-        return self.product_id.price
-    
+
+class Note(models.Model):
+    note = models.TextField()
+    name = models.CharField(max_length=128)
+    date = models.DateTimeField(auto_now_add=True)
+
+class Receivable(models.Model):
+    active = models.BooleanField(default=False)
+    seller = models.CharField(max_length=128, blank=True, null=True)
+    client = models.CharField(max_length=128, blank=True, null=True)
+    total = models.DecimalField(max_digits=7, decimal_places=2)
+    date = models.DateField(auto_now_add=False)
+    number = models.CharField(max_length=128, blank=True, null=True)
