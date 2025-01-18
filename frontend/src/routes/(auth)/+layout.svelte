6b5e0746018c47d240eb3c12c00bb29ed905cbd7
Vyros17 <162827531+Vyros17@users.noreply.github.com>
2025-01-18 07:35:34 +0000

    Register

--- frontend/src/routes/(auth)/+layout.svelte
+++ frontend/src/routes/(auth)/+layout.svelte
@@ -1,7 +1,7 @@
 
-
-<div class="h-full w-full shadow-xl">
-    <div class="mx-auto my-[6rem] card p-[3rem] max-w-[45rem] text-center variant-ghost-surface shadow-xl">
+<!-- modificado de 6 a 3.75 porque el formulario de registro sobresalia la pantalla -->
+<div class="h-full w-full shadow-xl"> 
+    <div class="mx-auto my-[3.75rem] card p-[3rem] max-w-[45rem] text-center variant-ghost-surface shadow-xl">
         <slot></slot>
     </div>
 </div>
