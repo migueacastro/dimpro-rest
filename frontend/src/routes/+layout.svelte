5b45fda697720c235d4ecf52567b7ca5a39c3260
migueacastro <miguelcastropadron@gmail.com>
2025-01-20 23:09:37 -0400

    Fix dark mode finally

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,37 +1,32 @@
 <script lang="ts">
 	import '../app.postcss';
+	import { AppShell, AppBar, ProgressRadial } from '@skeletonlabs/skeleton';
+
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
-	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+	import { onMount } from 'svelte';
 	import { authenticate } from '$lib/auth';
 	import { user } from '../stores/stores';
-	import { onMount } from 'svelte';
-	import { ProgressRadial } from '@skeletonlabs/skeleton';
-	import '@fortawesome/fontawesome-free/css/all.min.css';
-	import { autoModeWatcher } from '@skeletonlabs/skeleton';
-	import { initializeStores } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 
-	initializeStores();
 
-	$: loaded = false;
-
-	onMount(async () => {
-		
-		autoModeWatcher();
-		let userData = await authenticate();
-		user.set(userData);
-		setTimeout(()=> loaded = true, 1000);
-	});
+    let loaded = false;
+    onMount(async () => {
+        let userData = await authenticate();
+        user.set(userData);
+        setTimeout(()=> loaded = true, 1000);
+    })
 </script>
-{#if loaded}
-	<slot/>
-{:else}
-	<div class="flex justify-center align-middle h-full">
-		<div class="my-auto">
-			<ProgressRadial />
-		</div>
-	</div>
+{#if !(loaded)}
+    <div class="flex justify-center mt-[15rem]">
+        <div class="my-auto">
+            <ProgressRadial/>
+        </div>
+    </div>
 {/if}
+<div class="flex h-full" class:hidden={!loaded}>
+    
+    <slot/>
+</div>
 
-<style lang="postcss" global></style>
