95e9514d0215ac719d41b15b95edee41577a4b68
migueacastro <miguelcastropadron@gmail.com>
2025-01-29 01:13:31 -0400

    Changing serializers for fitting auth groups

--- backend/dimpro/admin.py
+++ backend/dimpro/admin.py
@@ -3,15 +3,15 @@ from dimpro.models import *
 #
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin): # Esto es para ver detalladamente la lista
-  list_display = ('id', 'item', 'details', 'reference', 'prices', 'available_quantity')
+  list_display = ('id', 'item', 'details', 'reference', 'prices', 'available_quantity',)
   list_filter = ('active',)
-  search_fields = ('item', 'details', 'reference')
+  search_fields = ('item', 'details', 'reference',)
   list_per_page = 25 
 
 class UserAdmin(admin.ModelAdmin):
-  list_display = ('id', 'email', 'name', 'phonenumber', 'active', 'is_superuser')
-  list_filter = ('active', 'is_superuser')
-  search_fields = ('email', 'name')
+  list_display = ('id', 'email', 'name', 'phonenumber', 'active',)
+  list_filter = ('active',)
+  search_fields = ('email', 'name',)
   list_per_page = 25
 
 admin.site.register(User, UserAdmin)
