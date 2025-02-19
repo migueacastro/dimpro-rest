d05d95931323c968ec577277ba8c583de528a186
Vyros17 <162827531+Vyros17@users.noreply.github.com>
2025-02-19 18:11:18 +0000

    form component added

---
+++ frontend/src/lib/components/Form.svelte
@@ -0,0 +1,92 @@
+<script lang="ts">
+	//@ts-nocheck
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
+	import { getData } from '$lib/components/data';
+	import { formData } from '$lib/components/formData';
+
+	export let fields = [{ type: null, value: null, name: null, label: null }];
+	export let method = '';
+	export let endpoint = '';
+	export let edit = false;
+	let id = $page.params.id;
+	async function isEditable() {
+		if (edit) {
+			let data = await getData(endpoint);
+			let value = data.filter((values: { id: string }) => values.id == id);
+			for (let i = 0; i < fields.length; i++) {
+				fields[i].value = value[0][`${fields[i].name}`];
+			}
+		}
+	}
+	async function sendData() {
+		let body = {};
+		fields.forEach((field) => {
+			body[field.name] = field.value;
+		});
+		let response = await formData(endpoint, body, method);
+	}
+
+	onMount(async () => {
+		await isEditable();
+	});
+</script>
+
+<form class=" gap-10 flex flex-col lg:flex-row">
+	<div class="card my-3 p-10 text-start lg:w-[75%] space-y-6">
+		{#each fields as field}
+			<label
+				class="label"
+				for={field?.type == 'object' ? 'file' : field?.name}
+				class:hidden={field?.type === 'hidden'}
+			>
+				<p class="capitalize">{field.label}</p>
+				{#if field?.type === 'text'}
+					<input class="input" type="text" bind:value={field.value} id={field.name} />
+				{:else if field?.type === 'password'}
+					<input class="input" type="password" bind:value={field.value} id={field.name} />
+				{:else if field?.type === 'decimal'}
+					<input
+						class="input w-[25%]"
+						type="number"
+						min="0"
+						step="0.01"
+						bind:value={field.value}
+						id={field.name}
+					/>
+				{:else if field?.type === 'integer'}
+					<input
+						class="input w-[25%]"
+						type="number"
+						min="0"
+						bind:value={field.value}
+						id={field.name}
+					/>
+				{:else if field?.type === 'boolean'}
+					<p>bool</p>
+				{:else if field?.type === 'hidden'}
+					<input class="input" type="hidden" bind:value={field.value} id={field.name} />
+				{:else if field?.type === 'date'}
+					<input class="input" type="date" bind:value={field.value} id={field.name} />
+				{:else if field?.type === 'datetime'}
+					<input
+						class="input"
+						type="datetime-local"
+						placeholder=""
+						bind:value={field.value}
+						id={field.name}
+					/>
+				{:else if field?.type === 'textarea'}
+					<textarea class="textarea" bind:value={field.value} id={field.name} />
+				{:else if field?.type == 'foreignKey'}
+					<p>algo foraneo</p>
+				{:else if field?.type == 'manyToMany'}
+					<p>algo</p>
+				{/if}
+			</label>
+		{/each}
+		<button type="submit" class="btn variant-filled h-fit w-fit mx-auto btn-xl" on:click={sendData}
+			>Guardar</button
+		>
+	</div>
+</form>
--- frontend/src/lib/components/data.ts
+++ frontend/src/lib/components/data.ts
@@ -1,5 +1,7 @@
 import Cookies from "js-cookie";
+import { apiURL } from "$lib/api_url";
 export async function getData(endpoint: string): Promise<any> {
+	endpoint = apiURL + endpoint;
 	const token = Cookies.get("token");
     const response = await window.fetch(endpoint, {
         method: 'GET',
