bc950037340fbff417e142b4ba7aa15c2b6095df
Vyros17 <162827531+Vyros17@users.noreply.github.com>
2025-02-23 05:38:25 +0000

    progress

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -80,16 +80,16 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-     "default": {
-         "ENGINE": "django.db.backends.sqlite3",
-         "NAME": BASE_DIR / "db.sqlite3",
-    #"default": {
-    #    "ENGINE":"django.db.backends.postgresql_psycopg2",
-    #    "NAME": "postgres",
-    #    "USER": "postgres",
-    #    "PASSWORD": "2006",
-    #    "HOST": "localhost",
-    #    "PORT": "5432"
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    "default": {
+        "ENGINE":"django.db.backends.postgresql_psycopg2",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "2006",
+        "HOST": "localhost",
+        "PORT": "5432"
     }
 }
 
