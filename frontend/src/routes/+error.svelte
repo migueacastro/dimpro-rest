4af3be3caa80e5bc8bcde522947b7e9ef5f81ff3
migueacastro <miguelcastropadron@gmail.com>
2025-05-21 15:51:10 -0400

    Implementing permission checking, only lacking the orders page, adding files

---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script>
+  import {page} from '$app/state';
+</script>
+<div class="flex flex-col space-y-4 justify-center w-full text-center">
+  <h1 class="h1">{page.status} {page.error?.message}</h1>
+  <p class="p">Contacte al administrador</p>
+  <button class="btn variant-filled-primary w-fit mx-auto text-2xl" on:click={()=> {
+    window.history.back();
+  }}>Volver</button>
+</div>
