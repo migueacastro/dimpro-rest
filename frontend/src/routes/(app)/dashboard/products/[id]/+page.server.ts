c6ff29b78098e0b2f40a819e4125bca3cfcf9b02
migueacastro <miguelcastropadron@gmail.com>
2025-05-29 09:50:57 -0400

    Add product view and fix error within users and staff

---
+++ frontend/src/routes/(app)/dashboard/products/[id]/+page.server.ts
@@ -0,0 +1,14 @@
+import { apiURL } from "$lib/api_url";
+import { checkPermission, permissionError } from "$lib/auth";
+import type { PageServerLoad } from "../../inventory/$types";
+
+export const load: PageServerLoad = async ({fetch, params, locals}: any) => {
+  if (!checkPermission(locals.user, 'view_product')) {
+    return permissionError();
+  }
+  const response = await fetch(apiURL+'products/'+ params.id);
+  const product = await response.json();
+  return {
+    product,
+  }
+};
