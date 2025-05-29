9fa60a0feff41867ffed53e437389caa21652cc5
migueacastro <miguelcastropadron@gmail.com>
2025-05-28 22:11:58 -0400

    Fixed error with updating user, again

--- frontend/src/routes/(app)/dashboard/user/edit/[id]/+page.svelte
+++ frontend/src/routes/(app)/dashboard/user/edit/[id]/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { checkStaffGroup } from '$lib/auth.js';
 	import Form from '$lib/components/Form.svelte';
 	export let data;
 	let {user} = data;
@@ -9,4 +10,4 @@
 		];
 </script>
 
-<Form fields={fields} edit={true} user={user} endpoint={'users'} reload={true} table_name={'usuario'}/>
+<Form fields={fields} edit={true} user={user} endpoint={(checkStaffGroup(user)) ? 'staff' : 'users'} reload={true} table_name={'usuario'}/>
